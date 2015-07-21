b408a67956c719b0d94fe4952025074663949108
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-07-21 15:19:07 +0900

    Integrate rejs module

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const rejs = require('rejs');
 module.exports = function () {
   this.filter("transformerName", (source, options) => {
+    const resolver = new rejs.Resolver(options || {});
     try {
       return source
      } catch (e) { throw e }
